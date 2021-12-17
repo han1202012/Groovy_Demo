0a088e8ca3007000bc83618bba695d583b08085f
hanshuliang <octopus_truth@163.com>
2021-12-17 20:11:22 +0800

    Initial commit

---
+++ src/main/groovy/Test.groovy
@@ -0,0 +1,10 @@
+// Groovy 类前可以不加 public
+class Test {
+    // 函数参数类型 , 可以不用声明
+    static void main(args) {
+        // Groovy 代码可以不使用分号
+        // println 方法可以直接调用 , 不使用 System.out 也可以
+        // println 后与要打印的字符串 使用空格隔开即可 , 可以不使用括号
+        println "Hello Groovy!!!"
+    }
+}
